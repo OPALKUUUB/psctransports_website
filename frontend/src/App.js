85d38bff51ac5ffe8089cab48a0d2a4dff880b91
OPALKUUUB <63914959+OPALKUUUB@users.noreply.github.com>
2021-09-28 22:00:07 +0700

    add section Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,44 +2,56 @@ import React, { Component } from "react";
 import "../node_modules/react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
 
-import NavBar from "./components/NavBar";
+import Navbar from "navbar-react";
 import { Carousel } from "react-responsive-carousel";
 import Home from "./components/Home";
 
-const navObj = {
-  logo: "PSC",
-  option: ["home", "suggestion", "contact"],
-};
 class App extends Component {
+  static defaultProps = {
+    navBar: {
+      item: [
+        { name: "home", href: "#home" },
+        { name: "suggestion", href: "#suggestion" },
+        { name: "contact us", href: "#contact" },
+      ],
+      color: "#2F5D62",
+      logo: "/image/logo3.png",
+    },
+  };
   render() {
     return (
       <div className="App">
-        <NavBar obj={navObj} />
-        <div style={{ marginTop: "-5px" }}>
-          <Carousel
-            autoPlay
-            showStatus={false}
-            showThumbs={false}
-            useKeyboardArrows
-            verticalSwipe={"standard"}
-            stopOnHover>
-            <div>
-              <img src="/image/img1.1.1.jpg" />
-            </div>
-            <div>
-              <img src="/image/img3.3.3.jpg" />
-            </div>
-            <div>
-              <img src="/image/img2.2.2.jpg" />
-            </div>
-          </Carousel>
-        </div>
+        <Navbar
+          items={this.props.navBar.item}
+          color={this.props.navBar.color}
+          logo={this.props.navBar.logo}
+          hoverColor="#DFEEEA"
+          titleColor="#DFEEEA"
+          title="หจก. ปัญญาวดี เซอร์วิส แอนด์ คอนซัลแตนท์"
+        />
+        <Carousel
+          autoPlay
+          showStatus={false}
+          showThumbs={false}
+          useKeyboardArrows
+          verticalSwipe={"standard"}
+          stopOnHover>
+          <div>
+            <img src="/image/carousel1.jpg" />
+          </div>
+          <div>
+            <img src="/image/carousel2.jpg" />
+          </div>
+          <div>
+            <img src="/image/carousel3.jpg" />
+          </div>
+        </Carousel>
         <div className="background-base">
           <section id="home">
             <Home />
           </section>
-          <section id="suggestion">test suggestion</section>
-          <section id="contact">test contact</section>
+          <section id="suggestion">section suggestion!!!</section>
+          <section id="contact">section contact us!!!</section>
         </div>
       </div>
     );
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,21 +10,26 @@ class Home extends Component {
         </div>
         <div className="body">
           <div className="left">
-            <h3>บริษัทเราทำอะไร</h3>
-            <p>
+            <h3 className="left-head">บริษัทเราทำอะไร</h3>
+            <p className="left-body">
               บริการรถ 6 ล้อ 10 ล้อ ขนส่งตู้คอนเทนเนอร์ ทุกจังหวัดทั่วไทย
               ไม่ว่าจะเป็นการนำเข้าตู้คอนเทนเนอร์จากท่าเรือแหลมฉบังไปเปิดที่โรงงาน
               การส่งออกตู้คอนเทนเนอร์จากโรงงานไปท่าเรือ
             </p>
-            <h3>สิ่งที่คุณจะได้รับ</h3>
-            <p>
+            <h3 className="left-head">สิ่งที่คุณจะได้รับ</h3>
+            <p className="left-body">
               การขนส่งสินค้าที่ รวดเร็ว ปลอดภัย
               เราสามารถให้ความมั่นใจกับบริการของเรา
               ด้วยประสบการณ์ในการดำเนินกิจการที่มากกว่า 10 ปี รถทุกคันมี GPS และ
               คนขับที่มีประสิทธิภาพ
             </p>
           </div>
-          <div className="right">image content right!!!</div>
+          <div className="right-body">
+            <img src="/image/home1.jpg" />
+            <img src="/image/home2.jpg" />
+            <img src="/image/home3.jpg" />
+            <img src="/image/home4.jpg" />
+          </div>
         </div>
       </div>
     );
